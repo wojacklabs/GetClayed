f660639ba03a1186a5fdee3e32970589377f13a5
wonseokchoi <wonseokchoi@nts-corp.com>
2025-11-15 18:19:31 +0900

    update

--- components/AddLibraryModal.tsx
+++ components/AddLibraryModal.tsx
@@ -112,9 +112,7 @@ export default function AddLibraryModal({
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 p-4">
-      <div className="absolute inset-0 bg-black bg-opacity-50" onClick={onClose} />
-      
-      <div className="relative bg-white rounded-lg shadow-xl max-w-md w-full p-6">
+      <div className="relative bg-white rounded-lg shadow-xl max-w-md w-full p-6 border border-gray-200">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 p-1 hover:bg-gray-100 rounded-lg transition-colors"
